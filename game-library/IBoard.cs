b29170a5231a613f78b2d974514f86c03964ce9b
Matthew Morgan <mmorgan55@cnm.edu>
2019-09-14 15:19:20 -0600

    Add project files.

---
+++ game-library/IBoard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game_library
+{
+    public interface IBoard
+    {
+        void MakeBoard();
+    }
+}
---
+++ game-library/ITile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace game_library
+{
+    public interface ITile
+    {
+        string StringRepresentation { get; set; }
+    }
+}
